dae20d718e742fe1147eb52ec39a60640e9a79e5
SeungKyu <steelblue.lee@gmail.com>
2018-07-17 18:11:32 +0900

    2018-07-17 18:11:32.171123

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -1,7 +1,7 @@
 function fish_user_key_bindings
-  fzf_key_bindings
   fish_vi_key_bindings
   bind -M insert -m default jk backward-char force-repaint
-  bind -M default H beginning-of-line accept-autosuggestion
-  bind -M default L end-of-line accept-autosuggestion
+  bind -M default H beginning-of-line
+  bind -M default L end-of-line
+  fzf_key_bindings
 end
